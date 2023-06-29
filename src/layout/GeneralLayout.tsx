a44c6db5bdc8f9b132a0c52f0bcb681894143e95
ShinDongJae <sdhsdj6450@gmail.com>
2023-06-29 16:45:21 +0900

    feat: page redirection

--- src/layout/GeneralLayout.tsx
+++ src/layout/GeneralLayout.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import useAuthRedirection from '../hooks/useAuthRedirection';
+import Path from '../utils/constants/path';
 
 interface GeneralLayoutProps {
   children: React.ReactNode;
 }
 
 const GeneralLayout: React.FC<GeneralLayoutProps> = ({ children }) => {
+  const isAuth = useAuthRedirection({
+    to: Path.SIGNIN,
+    isRedirectionIfAuth: false,
+  });
+
+  if (!isAuth) {
+    return null;
+  }
   return <>{children}</>;
 };
 
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import useAuthRedirection from '../../hooks/useAuthRedirection';
+import Path from '../../utils/constants/path';
 const Signin = () => {
+  const isAuth = useAuthRedirection({
+    to: Path.TODO,
+    isRedirectionIfAuth: true,
+  });
+
+  if (isAuth) {
+    return <></>;
+  }
   return <div>Signin</div>;
 };
 
