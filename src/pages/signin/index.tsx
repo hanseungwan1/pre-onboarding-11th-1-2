64d6f93b79d9f69ddf1c8b7fad753191e85b0f9a
ShinDongJae <sdhsdj6450@gmail.com>
2023-06-30 11:14:56 +0900

    refactor: singin merge 후 코드정리

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import useAuthRedirection from '../../hooks/useAuthRedirection';
 import Path from '../../utils/constants/path';
 import { checkSignin } from './signin.hook';
-import { checkEmail, checkPassword } from './signin.hook';
-// import { useNavigate } from 'react-router-dom';
 import {
   Alert,
   AlertDescription,
@@ -16,24 +14,39 @@ import {
   Text,
 } from '@chakra-ui/react';
 
+import { checkEmail, checkPassword } from '../../utils/helper/validationCheck';
+import { useRouter } from '../../hooks/useRouter';
+
 const Signin = () => {
-  // const navigate = useNavigate();
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+
+  const { replaceTo } = useRouter();
+
   const isAuth = useAuthRedirection({
     to: Path.TODO,
     isRedirectionIfAuth: true,
   });
+
+  if (isAuth) {
+    return <></>;
+  }
+
   const EmailHanlder = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
+
   const PasswordHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
+
   const loginHandler = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    checkSignin(email, password);
+
+    const response = await checkSignin(email, password);
+    if (response) replaceTo(Path.TODO);
   };
+
   return (
     <Center h="90vh">
       <Box
@@ -56,7 +69,7 @@ const Signin = () => {
               onChange={EmailHanlder}
               value={email}
             />
-            {checkEmail(email) === 'fail' && (
+            {!checkEmail(email) && email.length > 0 && (
               <Alert status="error" h="30px" marginTop="5px">
                 <AlertIcon w="15px" />
                 <AlertDescription fontSize="10px">
@@ -75,7 +88,7 @@ const Signin = () => {
               value={password}
               type="password"
             />
-            {checkPassword(password) === 'fail' && (
+            {!checkPassword(password) && password.length > 0 && (
               <Alert status="error" h="30px" marginTop="5px">
                 <AlertIcon w="15px" />
                 <AlertDescription fontSize="10px">
@@ -96,10 +109,7 @@ const Signin = () => {
           marginBottom="20px"
           data-testid="signup-button"
           isDisabled={
-            checkEmail(email) === 'success' &&
-            checkPassword(password) === 'success'
-              ? false
-              : true
+            checkEmail(email) && checkPassword(password) ? false : true
           }
           onClick={loginHandler}
         >
--- src/pages/signin/signin.hook.ts
+++ src/pages/signin/signin.hook.ts
@@ -1,15 +1,15 @@
 import { signin } from '../../apis/auth';
 import { setLocalStorageToken } from '../../utils/auth';
 
-export const checkEmail = (email: string) => {
-  return email.indexOf('@') === -1 ? 'fail' : 'success';
-};
-
-export const checkPassword = (password: string) => {
-  return password.length < 8 ? 'fail' : 'success';
-};
 export const checkSignin = async (email: string, password: string) => {
-  const response = await signin(email, password);
-  setLocalStorageToken(response.data.access_token);
-  // navigate('/todo');
+  try {
+    const response = await signin(email, password);
+
+    if (response.status === 200) {
+      setLocalStorageToken(response.data.access_token);
+      return true;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
